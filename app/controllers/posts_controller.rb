950dc9f28d8dd10bcc28a76d4fb794f31461b3e5
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-09 12:00:58 +0900

    update: ルーティング設定削除及びcreate/topコントローラー追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
   def top
     @comment_new = Post.new
   end
+  def create
+    @comment_new = Post.new(post_params)
+    if @comment_new.save
+      redirect_to posts_path, notice: "作成完了"
+    else
+      render :new
+    end
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#top'
   get 'posts', to: 'posts#top'
-  post 'posts', to: 'posts#top'
   resources :posts do
     collection do
       post :confirm
