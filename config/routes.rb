b06bc9cd0e8b29f335d59051906d136fb3a3d47f
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-09 10:57:19 +0900

    update: routes.erb ルーティング設定追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'posts#top'
+  get 'posts', to: 'posts#top'
+  post 'posts', to: 'posts#top'
   resources :posts do
     collection do
       post :confirm
