90f5512e334f6ebf382480b9dc02ac783d0fc878
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-10 22:49:56 +0900

    update: インデント修正、posts.coffee削除、コントローラ間の改行追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,11 @@ class PostsController < ApplicationController
     @post = Post.new
     @posts = Post.all
   end
+
   def index
     redirect_to new_post_path
   end
+
   def create
     @post = Post.new(post_params)
     if params[:back]
@@ -13,26 +15,30 @@ class PostsController < ApplicationController
       render :new
     else
       if @post.save
-        redirect_to posts_path, notice: "ツイートされたった"
+        redirect_to posts_path, notice: "ヅイートされたった"
       else
         render :new
       end
     end
   end
+
   def confirm
     @posts = Post.all
     @post = Post.new(post_params)
     render :new if @post.invalid?
   end
+
   def destroy
     set_posts
     @post.destroy
     redirect_to posts_path, notice:"消しったー"
   end  
+
   def edit
     set_posts
     @posts = Post.all
   end
+
   def update
     @posts = Post.all
     set_posts
@@ -42,10 +48,13 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+
   private
+
   def post_params
     params.require(:post).permit(:content)
   end
+  
   def set_posts
     @post = Post.find(params[:id])
   end
